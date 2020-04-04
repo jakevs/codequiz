6842961960ccb8082b5a15a1677cccac65234e68
James Van Slyke <jakevs3@gmail.com>
2020-04-03 23:01:29 -0500

    adding pseudo code for js file

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+Create variables - begin button - 
+Create variable for questions - var question = Array
+Function to start the quiz - getElementById - addEventListener
+Function to go to next question
+Function to do something when button selected - timer starts
+- var playButton = document.querySelector("#play");
+
+- beginBtn.addEventListener("click", startTimer);
+Keep score
