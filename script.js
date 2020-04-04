8fcaa66014c746d8f5b668bfd8d6031c1c890c0c
James Van Slyke <jakevs3@gmail.com>
2020-04-04 13:18:20 -0500

    putting on click for timer

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 /* 
+When begin button is clicked, countdown starts and start answering questions
 Create variables - begin button - 
 Create variable for questions - var question = Array
+- var myQuestions = [
+	{
+		question: "What is the markup language of the internet?",
+		answers: {
+			a: 'Java',
+			b: 'HTML',
+			c: 'CSS'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What do you use to style a webpage?",
+		answers: {
+			a: 'HTML',
+			b: 'Javascript',
+			c: 'CSS'
+		},
+		correctAnswer: 'c'
+	}
+];
 Function to start the quiz - getElementById - addEventListener
 Function to go to next question
 Function to do something when button selected - timer starts
@@ -8,3 +29,21 @@ Function to do something when button selected - timer starts
 
 - beginBtn.addEventListener("click", startTimer);
 Keep score
+*/
+
+$( "#begin-btn").on("click", function() {
+    addEventListener.counter;
+
+var counter = 60;
+var interval = setInterval(function() {
+    counter--;
+    if (counter <= 0) {
+     		clearInterval(interval);
+      	$('#timer').html("<h3>Game Over!</h3>");  
+        return;
+    }else{
+    	$('#time').text(counter);
+      console.log("Timer --> " + counter);
+    }
+}, 1000);
+});
