d4828cbb1f5f26d9e26455f46a85e2be955f2118
James Van Slyke <jakevs3@gmail.com>
2020-04-05 02:42:09 -0500

    creating array for questions

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 When begin button is clicked, countdown starts and start answering questions
 Create variables - begin button - 
 Create variable for questions - var question = Array
-- var myQuestions = [
+- var questions = [
 	{
-		question: "What is the markup language of the internet?",
+		questions: "What is the markup language of the internet?",
 		answers: {
 			a: 'Java',
 			b: 'HTML',
@@ -22,15 +22,32 @@ Create variable for questions - var question = Array
 		correctAnswer: 'c'
 	}
 ];
+
+// $( "#first").on("click", function() {
+//     alert("Wrong!");
+// });
+
+// $( "#second").on("click", function() {
+//     alert("Wrong!");
+// });
+
+// $( "#third").on("click", function() {
+//     alert("Correct!");
+// });
+
 Function to start the quiz - getElementById - addEventListener
 Function to go to next question
 Function to do something when button selected - timer starts
 - var playButton = document.querySelector("#play");
 
 - beginBtn.addEventListener("click", startTimer);
-Keep score
+Get timer to stop when questions are done being answered
+Keep score - score is equal to time left
+List of highscores - save to localStorage.getItem('');
 */
 
+
+//Timer - starts on begin button
 $( "#begin-btn").on("click", function() {
     addEventListener.counter;
 
@@ -42,20 +59,43 @@ var interval = setInterval(function() {
       	$('#timer').html("<h3>Game Over!</h3>");  
         return;
     }else{
-    	$('#time').text(counter);
+    	$('#time').text("Timer: " + counter);
       console.log("Timer --> " + counter);
     }
 }, 1000);
 });
 
-$( "#first").on("click", function() {
-    alert("Wrong!");
-});
+//Selecting elements
+var questions = document.getElementById('question');
+var firstAnswer = document.getElementById('first');
+var secondAnswer = document.getElementById('second');
+var thirdAnswer = document.getElementById('third');
 
-$( "#second").on("click", function() {
-    alert("Wrong!");
-});
+//Creating questions array
+var questions = [
+    {
+        question : "What is the markup language for the internet?",
+        choiceA : "HTML",
+        choiceB : "CSS",
+        choiceC : "Java",
+        correct : "A"
+    },
+    {
+        question : "What dynamically changes a webpage?",
+        choiceA : "CSS",
+        choiceB : "Javascript",
+        choiceC : "HTML",
+        correct : "B"
+    },
+    {
+        question : "What is a library for Javascript?",
+        choiceA : "Linux",
+        choiceB : "Bootstrap",
+        choiceC : "JQuery",
+        correct : "C"
+    }
+];
 
-$( "#third").on("click", function() {
-    alert("Correct!");
-});
+//Creating variables
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
