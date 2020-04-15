ced12f7050fb8a24941fe3e2b91423ada745b1cd
James Van Slyke <jakevs3@gmail.com>
2020-04-14 21:53:52 -0500

    got score to equal time left on counter

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ List of highscores - save to localStorage.getItem('');
 */
 
 //Declaring Variables, pulling from Id's
-var questions = document.getElementById('question');
-var firstAnswer = document.getElementById('first');
-var secondAnswer = document.getElementById('second');
-var thirdAnswer = document.getElementById('third');
-var scores = document.getElementById('scores');
-var questionHolder = document.getElementById('question-holder');
+var questions = document.getElementById("question");
+var firstAnswer = document.getElementById("first");
+var secondAnswer = document.getElementById("second");
+var thirdAnswer = document.getElementById("third");
+var scores = document.getElementById("scores");
+var questionHolder = document.getElementById("question-holder");
 var counterEnd = 0;
 var score = 0;
 var currentQ = 0;
@@ -27,27 +27,27 @@ var counter = 60;
 
 //Creating questions array
 var questions = [
-    {
-        question: "What is the markup language for the internet?",
-        answerA: "HTML",
-        answerB: "CSS",
-        answerC: "Java",
-        correct: "A"
-    },
-    {
-        question: "What dynamically changes a webpage?",
-        answerA: "CSS",
-        answerB: "Javascript",
-        answerC: "HTML",
-        correct: "B"
-    },
-    {
-        question: "What is a library for Javascript?",
-        answerA: "Linux",
-        answerB: "Bootstrap",
-        answerC: "JQuery",
-        correct: "C"
-    }
+  {
+    question: "What is the markup language for the internet?",
+    answerA: "HTML",
+    answerB: "CSS",
+    answerC: "Java",
+    correct: "A",
+  },
+  {
+    question: "What dynamically changes a webpage?",
+    answerA: "CSS",
+    answerB: "Javascript",
+    answerC: "HTML",
+    correct: "B",
+  },
+  {
+    question: "What is a library for Javascript?",
+    answerA: "Linux",
+    answerB: "Bootstrap",
+    answerC: "JQuery",
+    correct: "C",
+  },
 ];
 
 //Creating variables
@@ -55,72 +55,70 @@ var lastQuestion = questions.length - 1;
 var runningQuestion = 0;
 var count = 0;
 var compareCount = 0;
+var interval;
 
 //Timer - starts on begin button
 $("#begin-btn").on("click", function () {
-    //    addEventListener.counter;
-    $('#question-holder').removeClass('visibility');
+  //    addEventListener.counter;
+  $("#question-holder").removeClass("visibility");
 
-
-    var interval = setInterval(function () {
-        counter--;
-        if (counter == 0) {
-            gameOver();
-        }
-        $('#time').text("Timer: " + counter);
-
-    }, 1000);
-
-
+  interval = setInterval(function () {
+    counter--;
+    if (counter == 0) {
+      gameOver();
+    }
+    $("#time").text("Timer: " + counter);
+  }, 1000);
 });
 
-function gameOver(score) {
-    var initials = prompt("Enter your initials!", "");
-    localStorage.setItem(initials, score);
-    $('#timer').html("<h3>Game Over!</h3>");
-    $('#question-holder').addClass('visibility');
-    $('#scores').html(`<h3>High Scores: ${initials} the score is ${score}</h3>`);
-    // clearInterval(interval); 
+function gameOver() {
+  clearInterval(interval);
+  var initials = prompt("Enter your initials!", "");
+  localStorage.setItem(initials, counter);
+  //   $("#timer").html("<h3>Game Over!</h3>");
+  $("#question-holder").addClass("visibility");
+  $("#scores").html(
+    `<h3>High Scores: ${initials} the score is ${counter}</h3>`
+  );
 }
 // Functions
 function renderQuestion() {
-    var q = questions[runningQuestion];
-    currentQ++;
-    question.innerHTML = "<p>" + q.question + "</p>";
-    firstAnswer.innerHTML = q.answerA;
-    secondAnswer.innerHTML = q.answerB;
-    thirdAnswer.innerHTML = q.answerC;
-    // console.log(currentQ);
-
-};
+  var q = questions[runningQuestion];
+  currentQ++;
+  question.innerHTML = "<p>" + q.question + "</p>";
+  firstAnswer.innerHTML = q.answerA;
+  secondAnswer.innerHTML = q.answerB;
+  thirdAnswer.innerHTML = q.answerC;
+  // console.log(currentQ);
+}
 
 function checkAnswer(answer) {
-    compareCount++
-    if (answer === questions[runningQuestion].correct) {
-        score++;
-        // console.log(score);
+  compareCount++;
+  if (answer === questions[runningQuestion].correct) {
+    score++;
+    // console.log(score);
 
-        // document.text(scores);
-    } else {
-        alert('Wrong!');
-    }
-    count = 0;
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        renderQuestion();
-        // clearInterval();
-    }
-    if (compareCount == questions.length) {
-        gameOver(score);
-    }
-};
+    // document.text(scores);
+  } else {
+    counter -= 5;
+    // alert("Wrong!");
+  }
+  count = 0;
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+    // clearInterval();
+  }
+  if (compareCount == questions.length) {
+    gameOver();
+  }
+}
 
-// if (questions[runningQuestion + 1] == questions.length) {             
+// if (questions[runningQuestion + 1] == questions.length) {
 //     gameOver();
 //   // return;
 // }
 // Functions
-
 
 // function getScore() {
 //     scores.style.display = "block";
